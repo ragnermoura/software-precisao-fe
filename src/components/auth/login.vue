385356cd8b4ebb74faade5fe0bf232e783c5a27c
Ragner Moura <44169840+ragnermoura@users.noreply.github.com>
2023-03-15 20:18:18 +0000

    roda-pé

--- src/components/auth/login.vue
+++ src/components/auth/login.vue
@@ -1,24 +1,29 @@
 <template id="Complogin">
     <div class="row">
-        <div class="col-md-4 primeira_parte">
+        <div class="col-md-5 primeira_parte">
             <img src="../../assets/images/logo.svg" class="img-fluid logo" alt="">
 
             <div class="cotainer login-area">
                     <h3 class="text-left mt-3 boas-vindas-texto">Olá, bem-vindo!</h3>
                     <h5 class="text-left  sub-texto">Prepare tudo para começarmos :)</h5>
-                    <p class="mensagem mt-3"><i class="fa fa-check ml-3"></i> Deslogado com sucesso</p>
-                    <!-- <p class="mensagem-error"><i class="fa fa-ban ml-3"></i> Erro de autenticação</p> -->
+                    <p hidden class="mensagem mt-3"><i class="fa fa-check ml-3"></i> Deslogado com sucesso</p>
+                    <p hidden class="mensagem-error"><i class="fa fa-ban ml-3"></i> Erro de autenticação</p>
+
                     <div class="mt-4">
                         <div class="input-icons">
                             <i class="fa fa-envelope"></i>
                             <input class="form-control form-control-lg" autocomplete="off" type="email" placeholder="email@dominio.com.br"
                                 aria-label=".form-control-lg example">
                         </div>
+                        <p hidden class="mensagem-error mt-3"><i class="fa fa-ban ml-3"></i> E-mail errado ou não existe :(</p>
+
                         <div class="input-icons">
                             <i class="fa fa-lock"></i>
                             <input class="form-control form-control-lg mt-2" autocomplete="off" type="password"
                                 placeholder="Digite sua Senha" aria-label=".form-control-lg example">
                         </div>
+                        <p hidden class="mensagem-error mt-3"><i class="fa fa-ban ml-3"></i> Senha errada ou não existe :(</p>
+
 
                         <div class="d-grid gap-2 mt-3 mb-2">
                             <button class="btn btn-primary" type="button">Entrar</button>
@@ -30,11 +35,12 @@
                         <p class="text-separate">ou acesse usando o <strong>app</strong></p>
                         <qrcode-vue class="qr-area" :value="value" :size="size" level="H" />
                     </div>
+                    
                 </div>
-
+                <Footer/>
             
         </div>
-        <div class="col-md-8 segunda_parte elemento-1">
+        <div class="col-md-7 segunda_parte elemento-1">
             <RouterLink to="/" class="btn_voltar"> Voltar ao site</RouterLink>
                 <img src="../../assets/images/img1.svg" class="img" />
                 <p class="text-login mb-3">Chegou a hora de otimizarmos o seu tempo. O Software Precisão deixa as
@@ -49,6 +55,7 @@
 </template>
 <script>
 import QrcodeVue from 'qrcode.vue'
+import Footer from '../footer/footer.vue'
 
 export default {
     name: 'Complogin',
@@ -60,6 +67,7 @@ export default {
     },
     components: {
         QrcodeVue,
+        Footer
     }
 
 }
--- src/views/dasboard/auth/Login.vue
+++ src/views/dasboard/auth/Login.vue
@@ -1,16 +1,14 @@
 <template id="login">
     <Complogin />
-   
 </template>
 <script>
 import Complogin from '../../../components/auth/login.vue'
-import Footer from '../../../components/footer/footer.vue'
+
 
 export default {
     name: 'login',
     components: {
         Complogin,
-        Footer
     }
 }
 </script>
