1769ed17cfe83d2fc5ecca67cf8b4658063a49e7
Ragner Moura <44169840+ragnermoura@users.noreply.github.com>
2023-03-16 23:31:49 +0000

    Tela do planos

--- src/views/dasboard/payment/Plan.vue
+++ src/views/dasboard/payment/Plan.vue
@@ -1,19 +1,19 @@
 <template id="Plan">
     <Plano />
     
-    <Footer />
+    
    
 </template>
 <script>
 import Plano from '../../../components/payment/plan.vue';
-import Footer from '../../../components/footer/footer.vue'
+
 
 
 export default {
     name: 'Plan',
     components: {
         Plano,
-        Footer
+    
        
     }
 }
